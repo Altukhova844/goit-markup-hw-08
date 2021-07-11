e62977866c4cb67bd21b1aea5ab674587e8d86ed
Aleksandra Altukhova <hovalex@gmail.com>
2021-07-11 17:50:59 +0300

    desparate-commit

---
+++ mobile-menu.js
@@ -0,0 +1,13 @@
+  
+(() => {
+  const refs = {
+    openMenuBtn: document.querySelector('.mobile-open'),
+    closeMenuBtn: document.querySelector('.mobile-close'),
+    menu: document.querySelector('.mobile'),
+  };
+  refs.openMenuBtn.addEventListener('click', toggleMenu);
+  refs.closeMenuBtn.addEventListener('click', toggleMenu);
+  function toggleMenu() {
+    refs.menu.classList.toggle('is-hidden');
+  }
+})();
